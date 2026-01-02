2b1b7ca346c7dfe57bc6c5c3712f649accadde13
JoostWG <122867812+JoostWG@users.noreply.github.com>
2026-01-02 23:53:44 +0100

    :recycle: Refactor ``expectValid`

--- src/test/helpers.ts
+++ src/test/helpers.ts
@@ -1,16 +1,8 @@
 import type { ValidatorFunc } from '../types';
 import { ValidationError } from '../ValidationError';
 
-export function expectValid(validate: ValidatorFunc, value: unknown, toBe?: unknown): void {
-    const expected = toBe ?? value;
-    const testCase = expect(validate(value, 'root'));
-
-    if (typeof expected === 'object' && expected !== null) {
-        testCase.toMatchObject(expected);
-        return;
-    }
-
-    testCase.toBe(expected);
+export function expectValid(validate: ValidatorFunc, value: unknown, expected?: unknown): void {
+    expect(validate(value, 'root')).toEqual(expected ?? value);
 }
 
 export function expectInvalid(validate: ValidatorFunc, value: unknown, message: string): void {
